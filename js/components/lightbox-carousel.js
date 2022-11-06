9f3e22e98dcc8005507d36ac75dffd3cb60947ec
Paweł <pawel_gnat@o2.pl>
2022-11-06 13:43:32 +0100

    feat: mobile product carousel

--- js/components/lightbox-carousel.js
+++ js/components/lightbox-carousel.js
@@ -1,17 +1,32 @@
 const productImages = document.querySelectorAll('.product-image__main > img')
 const [previousImageBtn, nextImageBtn] = document.querySelectorAll('.product-image__btn')
 const productLightbox = document.querySelector('.product-image__main')
+let currentImage = 0
 
 function showPreviousImage() {
-	// console.log('prev')
-	productLightbox.style.translate = '100%'
+	currentImage--
+
+	if (0 >= currentImage) {
+		currentImage = 0
+	}
+
+	changeImage(currentImage)
 }
 
 function showNextImage() {
-	// console.log('next')
+	currentImage++
 
-	productLightbox.style.translate = '-100%'
+	if (productImages.length - 1 < currentImage) {
+		currentImage = 0
+	}
+
+	changeImage(currentImage)
 }
+
+function changeImage(slide) {
+	productImages.forEach((image, index) => (image.style.translate = `${100 * (index - slide)}%`))
+}
+changeImage(currentImage)
 
 previousImageBtn.addEventListener('click', showPreviousImage)
 nextImageBtn.addEventListener('click', showNextImage)
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 import * as mobileMenu from './components/mobile-menu.js'
 import * as cart from './components/cart.js'
-import * as lightbox from './components/lightbox.js'
+import * as lightbox from './components/lightbox-carousel.js'
